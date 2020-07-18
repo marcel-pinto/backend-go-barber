f392532479d75e277126caf4c07582431a344e89
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-07-18 18:59:47 -0300

    Added cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, NextFunction, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -9,6 +10,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+// app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -32,5 +34,5 @@ app.use(
 );
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333');
+  console.log('🚀 Server started on port 3333');
 });
