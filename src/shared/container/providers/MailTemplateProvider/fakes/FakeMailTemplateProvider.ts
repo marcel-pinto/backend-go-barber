2c6e900825a286efaaf83198135492f0362efc07
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-10-15 08:10:41 -0300

    Template engine

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
 
 export default class FakeMaillTemplateProvider
   implements IMailTemplateProvider {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail Content';
   }
 }
