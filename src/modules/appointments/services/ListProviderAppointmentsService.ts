5d6f2fbd5bd23b25a96cfb65eec51dbe86a1c25e
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-10-18 10:12:29 -0300

    Configurando cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -15,6 +16,9 @@ export default class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -31,6 +35,9 @@ export default class ListProviderAppointmentsService {
         year,
       },
     );
+    // const data = await this.cacheProvider.recover('asd');
+    // console.log(data);
+    await this.cacheProvider.save('asd', 'asd');
 
     return appointments;
   }
