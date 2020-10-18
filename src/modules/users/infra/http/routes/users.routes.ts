cdc91ccfa56d01f8116998fdbb44f9ec3827bcd7
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-10-17 21:04:25 -0300

    Upload de arquivos para o Amazon S3

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -11,7 +11,7 @@ import ensureAuthentication from '../middlewares/ensureAuthentication';
 const usersRouter = Router();
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
   '/',
